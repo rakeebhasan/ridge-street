a245c633d8d0c546dcb593a74111bd54dd19e923
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-07-26 13:46:43 +0600

    fix some responsive issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,18 @@
-(function($){
-	$(document).ready(function() {	
-
+(function ($) {
+	$(document).ready(function () {
 		// Scroll to Top
-		jQuery('.scrolltotop').click(function(){
-			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+		jQuery(".scrolltotop").click(function () {
+			jQuery("html").animate({ scrollTop: "0px" }, 400);
 			return false;
 		});
-		
-		jQuery(window).scroll(function(){
+
+		jQuery(window).scroll(function () {
 			var upto = jQuery(window).scrollTop();
-			if(upto > 500) {
-				jQuery('.scrolltotop').fadeIn();
+			if (upto > 500) {
+				jQuery(".scrolltotop").fadeIn();
 			} else {
-				jQuery('.scrolltotop').fadeOut();
+				jQuery(".scrolltotop").fadeOut();
 			}
 		});
-
-
-		
-
-				
-		
-		
-		
-		
-		
-		
-		
-		
 	});
 })(jQuery);
